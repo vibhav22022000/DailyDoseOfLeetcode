2b91649622c90ddf104a89ed4480660834ac7525
Vibhav Rane <56669613+vibhav22022000@users.noreply.github.com>
2025-10-03 11:27:20 -0500

    Time: 2 ms (59.38%), Space: 148 MB (97.92%) - LeetHub

--- 0268-missing-number/0268-missing-number.dart
+++ 0268-missing-number/0268-missing-number.dart
@@ -1,8 +1,7 @@
 class Solution {
   int missingNumber(List<int> nums) {
     int n = nums.length;
-
-    int expectedSum = n * (n+1) ~/ 2;
+    int expectedSum = n * (n+1) ~/2;
     int actualSum = nums.reduce((a,b) => a+b);
 
     return expectedSum - actualSum;
