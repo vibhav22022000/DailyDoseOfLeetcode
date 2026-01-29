4deff4f3aa4c016deedcdab04c4ee885b701b212
Vibhav Rane <56669613+vibhav22022000@users.noreply.github.com>
2026-01-29 13:26:11 -0600

    Time: 23 ms (6.66%), Space: 21.3 MB (9.17%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,9 +1,21 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        count = defaultdict(int)
+        #Boyer-Moore Voting Algorithm
+        # result, count = 0,0
 
-        for num in nums:
-            count[num] += 1
-            if count[num] > len(nums) // 2:
-                return num
+        # for n in nums:
+        #     if count == 0:
+        #         result = n
+        #     count = (1 if n == result else -1)
+        # return result
         
+        #HASHMAP
+        count = {}
+        result, maxCount = 0,0
+
+        for n in nums:
+            count[n] = 1 + count.get(n,0)
+            result = n if count[n] > maxCount else result
+            maxCount = max(count[n], maxCount)
+        
+        return result
